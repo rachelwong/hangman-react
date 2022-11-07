32e86829043bb4ba48eef9faa9b7e3372d9ee738
rachel-wong <hello.rachelwong@gmail.com>
2022-11-07 11:44:11 +1000

    feat: built drawing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import { useState } from 'react'
-import './App.css'
+import words from './wordList.json'
+import HangmanDrawing from './components/HangmanDrawing'
+import HangmanWord from './components/HangmanWord'
+import Keyboard from './components/Keyboard'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)]
+  })
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
   return (
-    <div className="App">
-      <h1>Hello</h1>
-
+    <div style={{
+      maxWidth: "800px",
+      display: "flex",
+      flexDirection: "column",
+      gap: "2rem",
+      margin: "0 auto",
+      alignItems: "center",
+    }}>
+      <div style={{ fontSize: "2rem", textAlign: "center" }}>Lose Win</div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
     </div>
   )
 }

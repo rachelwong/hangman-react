628af0576f51a8334982a7a3faa0ba6be29009be
rachel-wong <hello.rachelwong@gmail.com>
2022-11-15 15:26:17 +1000

    feat: working game

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import words from './wordList.json'
 import HangmanDrawing from './components/HangmanDrawing'
 import HangmanWord from './components/HangmanWord'
@@ -11,7 +11,25 @@ function App() {
   const [guessedLetters, setGuessedLetters] = useState<string[]>([])
 
   const incorrectLetters = guessedLetters.filter((letter) => !wordToGuess.includes(letter))
-  
+
+  function addGuessedLetter(letter: string) {
+    if (guessedLetters.includes(letter)) return
+    setGuessedLetters(currentLetters => [...currentLetters, letter])
+  }
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const key = e.key
+
+      if (!key.match(/^[a-z]$/)) return;
+      e.preventDefault()
+      addGuessedLetter(key)
+    }
+    document.addEventListener("keypress", handler)
+    return () => {
+      document.removeEventListener("keypress", handler)
+    }
+  }, [])
   return (
     <div style={{
       maxWidth: "800px",
@@ -23,7 +41,7 @@ function App() {
     }}>
       <div style={{ fontSize: "2rem", textAlign: "center" }}>Lose Win</div>
       <HangmanDrawing numberOfGuesses={incorrectLetters.length} />
-      <HangmanWord />
+      <HangmanWord guessedLetters={guessedLetters} wordToGuess={ wordToGuess } />
       <div style={{alignSelf: 'stretch'}}>
         <Keyboard />
       </div>
